0b121f55061f1fd8031f2e4b918eae7c133a018a
Khaleda Khan <khaleda.khan87@gmail.com>
2016-08-01 23:31:31 +0000

    triangle fun

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+attr_accessor :side_1,:side_2,:side_3
+
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if ((side_1 + side_2 > side_3) && (side_2 + side_3 > side_1) && (side_1 + side_3 > side_2)) && (side_1 > 0 && side_2 > 0 && side_3 > 0)
+      if side_1 == side_2 && side_2 == side_3
+        return :equilateral
+      elsif side_1 != side_2 && side_2 != side_3 && side_1 != side_3
+        return :scalene
+      elsif (side_1 == side_2 && side_1 != side_3) || (side_1 == side_3 && side_2 != side_3) || (side_2 == side_3 && side_1 != side_3)
+        return :isosceles
+      end
+    else
+      raise TriangleError
+    end
+  end
+end
+
+
+class TriangleError < StandardError
+   "Not a valid triangle"
 end
